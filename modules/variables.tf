26c8a17c1c195a7d9d44768cfab9964617bbbcac
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-05-17 19:48:23 +0400

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -4,19 +4,19 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "pub_sub_cidr" {
+variable "public_subnet_cidr" {
   type        = string
   description = "Cidr_block of public subnet"
   default     = "10.0.1.0/24"
 }
 
-variable "priv_sub_cidr" {
+variable "private_subnet_cidr" {
   type        = string
   description = "Cidr_block of private subnet"
   default     = "10.0.2.0/24"
 }
 
-variable "az" {
+variable "availability_zones" {
   type        = list(string)
   description = "Availability Zones in Region"
   default     = ["us-east-1a", "us-east-1b"]
