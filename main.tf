012109114c367177d4cb04799b386c98c81b783f
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-05-17 20:03:30 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ resource "aws_instance" "public" {
 	ami                         = var.ami
 	instance_type               = var.instance_type
 	associate_public_ip_address = true
-	subnet_id   = module.my-vpc.pub_sub_id
+	subnet_id   = module.my-vpc.public_subnet_id
 	vpc_security_group_ids      = [module.my-vpc.security_group_id]
-}
-
-resource "aws_instance" "private" {
-	ami                    = var.ami
-	instance_type          = var.instance_type
-	subnet_id              = module.my-vpc.priv_sub_id
-	vpc_security_group_ids = [module.my-vpc.security_group_id]
 }
